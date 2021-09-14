f15c9b109f1162c7d1f31d5e487acd46da3622db
apple <s1070321@gm.pu.edu.tw>
2021-09-14 14:18:34 +0800

    2021/9/14 page3 需求者定位

--- app/src/main/java/com/example/easybus/Page3Activity.java
+++ app/src/main/java/com/example/easybus/Page3Activity.java
@@ -1,13 +1,21 @@
 package com.example.easybus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.telecom.Call;
 import android.util.Log;
 import android.view.View;
@@ -20,6 +28,18 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -27,9 +47,28 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Page3Activity extends AppCompatActivity {
-    String email,getmail;
+    String email, getmail;
     RequestQueue requestQueue;
     com.example.easybus.FloatingActionButton f;
+
+    String TAG = "Page3Activity";
+    int LOCATION_REQUEST_CODE = 10001;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(@NonNull LocationResult locationResult) {
+            if(locationResult == null){
+                return;
+            }
+            for(Location location: locationResult.getLocations()){
+                Log.d(TAG,"onLocationResult : "+location.toString());
+                Log.d(TAG,"getLongitude : "+location.getLongitude());
+                Log.d(TAG,"getLatitude : "+location.getLatitude());
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,65 +76,154 @@ public class Page3Activity extends AppCompatActivity {
 
         requestQueue = Volley.newRequestQueue(this);
         //抓email
-        SharedPreferences email = getSharedPreferences("email",MODE_PRIVATE);
-        getmail=email.getString("Email","");
+        SharedPreferences email = getSharedPreferences("email", MODE_PRIVATE);
+        getmail = email.getString("Email", "");
         //隱藏title bar
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         //跳頁到新增路線
-        Button btn1 = (Button)findViewById(R.id.btn1);
+        Button btn1 = (Button) findViewById(R.id.btn1);
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it1 = new Intent(Page3Activity.this,Page5011Activity.class);
+                Intent it1 = new Intent(Page3Activity.this, Page5011Activity.class);
                 //it1.putExtra("email",getmail);
                 startActivity(it1);
             }
         });
 
         //跳頁到搭車
-        Button btn2 = (Button)findViewById(R.id.btn2);
+        Button btn2 = (Button) findViewById(R.id.btn2);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it2 = new Intent(Page3Activity.this,Page61.class);
+                Intent it2 = new Intent(Page3Activity.this, Page61.class);
                 //it2.putExtra("email",getmail);
                 startActivity(it2);
             }
         });
 
         //跳頁到注意事項
-        Button btn3 = (Button)findViewById(R.id.btn3);
+        Button btn3 = (Button) findViewById(R.id.btn3);
         btn3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it3 = new Intent(Page3Activity.this,Page7Activity.class);
+                Intent it3 = new Intent(Page3Activity.this, Page7Activity.class);
                 //it3.putExtra("email",getmail);
                 startActivity(it3);
             }
         });
 
         //跳頁到我的帳戶
-        Button btn4 = (Button)findViewById(R.id.btn4);
+        Button btn4 = (Button) findViewById(R.id.btn4);
         btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent it4 = new Intent(Page3Activity.this,Page8Activity.class);
-               // it4.putExtra("email",getmail);
+                Intent it4 = new Intent(Page3Activity.this, Page8Activity.class);
+                // it4.putExtra("email",getmail);
                 // Toast.makeText(Page3Activity.this, getmail, Toast.LENGTH_SHORT).show();
                 startActivity(it4);
             }
         });
 
         //浮動按鈕撥打給緊急聯絡人
-        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 readUser();
             }
         });
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(4000);
+        locationRequest.setFastestInterval(2000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+    protected void onStop(){
+        super.onStop();
+        stopLocationUpdates();
+    }
+    protected void onStart() {
+        super.onStart();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            checkSettingsAndStartLocationUpdates();
+        } else {
+            askLocationPermission();
+        }
+    }
+
+    private void checkSettingsAndStartLocationUpdates() {
+        LocationSettingsRequest locationSettingsRequest = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build();
+        SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> locationSettingsResponseTask = client.checkLocationSettings(locationSettingsRequest);
+        locationSettingsResponseTask.addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                //Setting of device one satisfied and we can start location updates
+                startLocationUpdates();
+            }
+        });
+        locationSettingsResponseTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                if (e instanceof ResolvableApiException) {
+                    ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                    try {
+                        resolvableApiException.startResolutionForResult(Page3Activity.this, 1001);
+                    } catch (IntentSender.SendIntentException sendIntentException) {
+                        sendIntentException.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+    }
+    private void  stopLocationUpdates(){
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+    private void  getLastLocation(){
+
+    }
+    //要求定位權限
+    private void askLocationPermission(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED){
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                Log.d(TAG,"askLocationPermission: you should show an alert dialog");
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+            }else{
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},LOCATION_REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == LOCATION_REQUEST_CODE){
+            if(grantResults.length>0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                checkSettingsAndStartLocationUpdates();
+            }else{
+
+            }
+        }
     }
 
     @SuppressLint("LongLogTag")
---
+++ app/src/main/java/com/example/easybus/immediate_addressing.java
@@ -0,0 +1,13 @@
+package com.example.easybus;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class immediate_addressing extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+
+}
