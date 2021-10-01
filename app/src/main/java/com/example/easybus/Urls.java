6400053b2a6c0e7a0afaa820d4dde0eab67be493
Hsin_yingTest <s1071481@gm.pu.edu.tw>
2021-10-01 17:59:36 +0800

    211001 改改蓋蓋u!!!

--- app/src/main/java/com/example/easybus/Urls.java
+++ app/src/main/java/com/example/easybus/Urls.java
@@ -4,7 +4,7 @@ import java.net.UnknownHostException;
 
 public class Urls {
     //private static InetAddress ip;/////
-    public static String url1 = "http://192.168.1.102";
+    public static String url1 = "http://192.168.2.5";
     //public static String url1 = "http://8c708b26959c.ngrok.io";
     public static void main(String args[]) {
 
