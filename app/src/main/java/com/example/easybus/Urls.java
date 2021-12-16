a496cefd71425ff151e08175796b193319c6fa70
pupss95263 <s1070327@gm.pu.edu.tw>
2021-12-16 19:28:15 +0800

    20211216

--- app/src/main/java/com/example/easybus/Urls.java
+++ app/src/main/java/com/example/easybus/Urls.java
@@ -5,5 +5,5 @@ import java.net.UnknownHostException;
 public class Urls {
     //private local InetAddress ip IPv4
     //public static String url1 = "http://192.168.27.106";
-    public static String url1 = "http://120.110.112.63";
+    public static String url1 = "http://120.110.112.63";  
     }
